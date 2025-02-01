bc8f7def050f596f1312dfb82c1e5f5b3a6efc8d
human-toilet <maxibengochea67@gmail.com>
2025-01-31 22:16:06 -0500

    add env var

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
-import dotenv from 'dotenv'
+import { config } from 'dotenv'
 
-dotenv.config()
+config()
 
-export const { PORT, DB_USER, DB_PASSWORD, DB_NAME, SALT_ROUNDS, SECRET_TOKEN } = process.env
+//manejar las variables de entorno en un archivo de configuracion
+export const { PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_NAME, SALT_ROUNDS, SECRET_TOKEN } = process.env
