2fa6de52c1ec973d5bd6a14ba2d3712b2814a974
human-toilet <maxibengochea67@gmail.com>
2025-02-04 04:05:41 -0500

    clean code

--- src/config.ts
+++ src/config.ts
@@ -3,4 +3,10 @@ import { config } from 'dotenv'
 config()
 
 //manejar las variables de entorno en un archivo de configuracion
-export const { PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_NAME, SALT_ROUNDS, SECRET_TOKEN } = process.env
+export const { 
+  PORT, //puerto de escucha del servidor
+  POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_NAME, //variables de entorno relacionadas a posgres
+  SALT_ROUNDS, //saltos para hashear con becrypt
+  SECRET_TOKEN, //palabra secreta para tokenizar
+  MONGO_URL, MONGO_NAME //variables de entorno relacionadas a mongo
+ } = process.env
